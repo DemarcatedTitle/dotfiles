f3e520b30b786a6297f23a76f17b63bf139d5026
D <dimmton@gmail.com>
2018-06-25 22:06:13 -0700

    update installs

--- sudoinstalls.sh
+++ sudoinstalls.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-for i in zsh xsel neovim docker-compose nodejs npm silversearcher-ag; do
+# dos2unix may be required for wsl
+for i in zsh xsel neovim docker-compose nodejs npm silversearcher-ag autojump; do
     apt-get -y install $i
 done
